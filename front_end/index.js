5eaf90cc5a39bf0584e7ae922492a6b14923133b
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-01-05 11:32:36 -0500

    add react & es6 & webpack compile scripts

---
+++ front_end/index.js
@@ -0,0 +1,7 @@
+require("./style.scss")
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+let test = "hello";
+
+ReactDOM.render(<h1>Hello React!</h1>, document.getElementById('root'));
---
+++ front_end/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  cache: true,
+  entry: './index',
+  output: {
+    filename: 'browser-bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"]
+      }
+    ]
+  }
+};
